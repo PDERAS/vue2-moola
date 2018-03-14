c06e58e0ce8995658081f8ba8e862a7059dcc9a9
Cody Moorhouse <cody.moorhouse@pderas.com>
2018-03-14 12:14:14 -0600

    Update vue2-moola to install package differently (I.E. Support)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import Moola from './components/Moola.vue'
 
-export default {
-    install (Vue, options) {
-        Vue.component('moola', Moola);
-    }
+function install (Vue, options) {
+    Vue.component('moola', Moola);
 }
+
+export default install;
